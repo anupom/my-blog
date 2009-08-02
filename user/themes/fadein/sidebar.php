31021afffbe2dbaa713f2750480dd35d060cc029
anupom syam <anupom.syam@gmail.com>
2009-08-03 01:51:26 +0700

    changing icos to gif

--- user/themes/fadein/sidebar.php
+++ user/themes/fadein/sidebar.php
@@ -11,22 +11,22 @@
         <div class="vcol" id="findmein">
             <h3>find me in...</h3>
             <a title="Linked In" rel="external" href="http://www.linkedin.com/in/anupom">
-                <img alt="Linked In" src="<?php Site::out_url( 'theme' ); ?>/images/ico/linkedin.ico" />
+                <img alt="Linked In" src="<?php Site::out_url( 'theme' ); ?>/images/ico/linkedin.gif" />
             </a>
             <a title="Github" rel="external" href="http://github.com/anupom">
-                <img alt="Github" src="<?php Site::out_url( 'theme' ); ?>/images/ico/github.ico" />
+                <img alt="Github" src="<?php Site::out_url( 'theme' ); ?>/images/ico/github.gif" />
             </a>
             <a title="Twitter" rel="external" href="http://twitter.com/anupom">
-                <img alt="Twitter" src="<?php Site::out_url( 'theme' ); ?>/images/ico/twitter.ico" />
+                <img alt="Twitter" src="<?php Site::out_url( 'theme' ); ?>/images/ico/twitter.gif" />
             </a>
             <a title="Delicious" rel="external" href="http://delicious.com/anupom">
-                <img alt="Delicious" src="<?php Site::out_url( 'theme' ); ?>/images/ico/delicious.ico" />
+                <img alt="Delicious" src="<?php Site::out_url( 'theme' ); ?>/images/ico/delicious.gif" />
             </a>
             <a title="Slideshare" rel="external" href="http://www.slideshare.net/anupom98">
-                <img alt="Slideshare" src="<?php Site::out_url( 'theme' ); ?>/images/ico/slideshare.ico" />
+                <img alt="Slideshare" src="<?php Site::out_url( 'theme' ); ?>/images/ico/slideshare.gif" />
             </a>
             <a title="Google" rel="external" href="http://www.google.com.bd/search?q=anupom+syam">
-                <img alt="Google" src="<?php Site::out_url( 'theme' ); ?>/images/ico/google.ico" />
+                <img alt="Google" src="<?php Site::out_url( 'theme' ); ?>/images/ico/google.gif" />
             </a>
         </div>
         <?php $theme->flickrfeed(); ?>
